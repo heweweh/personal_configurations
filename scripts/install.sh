e0662d7e2bea62c8a1ca1cb71b47d7ea83e8b4ea
heweiwei <heweiwei@thundersoft.com>
2021-06-09 18:44:59 +0800

    install global from ppa

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,8 +2,8 @@
 
 # make sure nvim is up-to-date
 sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install neovim exuberant-ctags curl
+sudo add-apt-repository ppa:dns/gnu
+sudo apt install neovim global exuberant-ctags curl
 
 # donwload plug
 cd ~/.vim
@@ -12,20 +12,6 @@ git clone https://github.com/junegunn/vim-plug.git
 cd ~/.vim/autoload
 ln -s ../vim-plug/plug.vim
 
-# download global
-cd ~/.vim
-curl https://www.tamacom.com/global/global-6.6.6.tar.gz | tar xzv
-cd ~/.vim/global-6.6.6
-./configure
-make
-echo "installing GNU Global ..."
-sudo make install
-
-mkdir -p ~/.vim/manually-plugged/plugin
-cd ~/.vim/manually-plugged/plugin
-ln -s ~/.vim/global-6.6.4/gtags.vim
-ln -s ~/.vim/global-6.6.4/gtags-cscope.vim
-
 echo "installing fonts ..."
 sudo cp ~/.vim/vim/fonts/*.*tf /usr/share/fonts
 fc-cache
@@ -33,8 +19,7 @@ fc-cache
 # softlink for configuration files
 cd ~/
 ln -s ~/.vim/vim/vimrc .vimrc
-ln -s ~/.vim/global-6.6.4/gtags.conf .globalrc
-ln -s ~/.vim/vim/scripts/gtags-project-init
+ln -s ~/.vim/vim/vimrc .vim/init.vim
 
 # echo
 echo "NOTE: You can manully select fonts in preferences of your terminal."
