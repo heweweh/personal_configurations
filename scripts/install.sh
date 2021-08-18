8819fc2e15dd61067099ae9a905b3f59c15212b2
heweweh <hw-nick@163.com>
2021-08-18 16:58:02 +0800

    add script for neovim configuration.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,23 +5,25 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo add-apt-repository ppa:dns/gnu
 sudo apt install neovim global exuberant-ctags curl
 
-# donwload plug
-cd ~/.vim
-mkdir -p ~/.vim/autoload
-git clone https://github.com/junegunn/vim-plug.git
-cd ~/.vim/autoload
-ln -s ../vim-plug/plug.vim
-
 echo "installing fonts ..."
 sudo cp ~/.vim/vim/fonts/*.*tf /usr/share/fonts
 fc-cache
 
+# donwload plug
+mkdir -p ~/.vim/autoload
+cd ~/.vim
+git clone https://github.com/junegunn/vim-plug.git
+cd ~/.vim/autoload
+rm plug.vim
+ln -sv ../vim-plug/plug.vim
+
 # softlink for configuration files
 cd ~/
-ln -s ~/.vim/vim/vimrc .vimrc
-ln -s ~/.vim/vim/vimrc .vim/init.vim
-
-# echo
-echo "NOTE: You can manully select fonts in preferences of your terminal."
+rm .vimrc
+ln -sv .vim/vim/vimrc .vimrc
+rm .vim/init.vim
+ln -sv .vim/vim/vimrc .vim/init.vim
+rm .config/nvim # only if it's a soft link
+ln -sv .vim .config/nvim
 
 #done
